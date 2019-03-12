b5db07a11c3afaddf7d27bd1879f719d3bf1ff90
Benjamin Antin <bantin@stanford.edu>
2019-03-11 22:37:19 -0700

    Add bit_to_int and cleanup
    Change meta to be a struct.

--- src/compress.jl
+++ src/compress.jl
@@ -1,6 +1,15 @@
 module compress
 export naive_delta_code
+export decompress
+export Meta
 
+mutable struct Meta
+    nbits::Int
+    dims::Tuple
+    function Meta(nbits, dims)
+        return new(nbits, dims)
+    end
+end
 
 function naive_delta_code(data; nbits=8, verbose=false)
     # Flatten data
@@ -15,10 +24,7 @@ function naive_delta_code(data; nbits=8, verbose=false)
     # Setup output
     delta = BitArray(undef, nbits*len_str)
     outliers = Dict(1 => stream[1])
-    meta = Dict(
-        :nbits => nbits,
-        :dims => (n_streams, n_samples)
-    )
+    meta = Meta(nbits, (n_streams, n_samples))
     
 
     diff_min, diff_max = get_bin_size(nbits)
@@ -65,6 +71,24 @@ function int_to_bit(val, diff_min, nbits)
     binary_rep = bitstring(Int16(offset_val))
 
     return BitArray(c == '1' for c in binary_rep[16+1-nbits:16])
+end
+
+function bit_to_int(arr::Union{BitArray, BitVector})
+    @assert(length(arr) <= 64) # won't work for sizes > 64 bit ints!
+    reversed = arr.chunks[1]
+    return revbits(reversed)
+end
+
+# Taken from:
+# https://discourse.julialang.org/t/covert-bitarray-to-int64/9193/2
+function revbits(z::UInt64)
+    z = (((z & 0xaaaaaaaaaaaaaaaa) >>  1) | ((z & 0x5555555555555555) <<  1))
+    z = (((z & 0xcccccccccccccccc) >>  2) | ((z & 0x3333333333333333) <<  2))
+    z = (((z & 0xf0f0f0f0f0f0f0f0) >>  4) | ((z & 0x0f0f0f0f0f0f0f0f) <<  4))
+    z = (((z & 0xff00ff00ff00ff00) >>  8) | ((z & 0x00ff00ff00ff00ff) <<  8))
+    z = (((z & 0xffff0000ffff0000) >> 16) | ((z & 0x0000ffff0000ffff) << 16))
+    z = (((z & 0xffffffff00000000) >> 32) | ((z & 0x00000000ffffffff) << 32))
+    return z
 end
 
 end #endmodule
