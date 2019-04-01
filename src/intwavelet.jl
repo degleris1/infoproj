4d654fc0271581fc6244302ff7a6a1578ea121b0
Anthony Degleris <degleris1@gmail.com>
2019-04-01 10:03:22 -0400

    Some additions to the wavelet module

--- src/intwavelet.jl
+++ src/intwavelet.jl
@@ -1,11 +1,28 @@
+function wave_transform_data(data, res=16)
+    rows, cols = size(data)
+
+    wave_data = Int16.(zeros(rows, cols)
+    for r = 1:rows
+        println("Encoding row ", string(r))
+        c = 1
+        while (c <= cols)
+            trans_size = Int(min(2^res, 2^floor(log2(cols+1-c))))
+            wave_data[r, c:c+trans_size-1] = int_haar_transform(data[r, c:c+trans_size-1])
+            c += trans_size
+        end
+    end
+    
+    return wave_data
+end
+
 
 
 
 function int_haar_transform(signal)
     siglen = length(signal)
-
+    
     # Catch base 2 error
-    if (round(log2(siglen)) != log2(siglen))
+    if (floor(log2(siglen)) != log2(siglen))
         print("Need signal with length = 2^n")
         return
     end
@@ -27,10 +44,10 @@ function int_haar_transform(signal)
     out[1:Int(siglen/2)] = diffs
 
     # Step 3: generate and transform remainder (RECURSE)
-    resid = sigo + round.(diffs / 2)
+    resid = sigo + floor.(diffs / 2)
     out[Int(siglen/2+1):siglen] = int_haar_transform(resid)
 
-    return Int.(out)
+    return Int16.(out)
 end
 
 
@@ -50,7 +67,7 @@ function inv_int_haar_transform(signal)
         diff = signal[1]
         resid = signal[2]
 
-        sigo = resid - round(diff / 2)
+        sigo = resid - floor(diff / 2)
         sige = sigo - diff
 
         out[1] = sigo
@@ -62,7 +79,7 @@ function inv_int_haar_transform(signal)
         resid = inv_int_haar_transform(signal[Int(siglen/2+1):siglen])
     
 
-        sigo = resid - round.(diff / 2)
+        sigo = resid - floor.(diff / 2)
         sige = sigo - diff
 
         out[1:2:end] = sigo
